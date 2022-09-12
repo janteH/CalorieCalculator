15827cd047a7719b462d6f10504d589a90801d94
Jante <jante.wallin@gmail.com>
2022-09-12 20:57:25 +0300

    valmis

---
+++ calculator/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [paino, setPaino] = useState(0);
+  const [intensiteetti, setIntensiteetti] = useState(0);
+  const [sukupuoli, setSukupuoli] = useState("mies");
+  const [tulos, setTulos] = useState(0);
+
+  const laske = () => {
+    let kalorit = 0;
+    if (sukupuoli === 'mies') {
+      kalorit = (879 + 10.2 * paino) * intensiteetti;
+    } else {
+      kalorit = (795 + 7.18 * paino) * intensiteetti;
+    }
+    setTulos(kalorit);
+  }
+
+
+/*   function laske(e) {
+    //e.preventDefault();
+    let kalorit = 0;
+    if (sukupuoli === 'mies') {
+      kalorit = (879 + 10.2 * paino) * intensiteetti;
+    } else {
+      kalorit = (795 + 7.18 * paino) * intensiteetti;
+    }
+    setTulos(kalorit);
+  } */
+
+
+  return (
+  /*  <form onSubmit={laske}> */
+    <form>
+     <h3>Laske kaloreiden kulutus</h3>
+     <div>
+      <label>Paino</label>
+      <input type="number" value={paino} onChange={e => setPaino(e.target.value)} />
+     </div>
+     <div>
+       <label>Intensiteetti</label>
+       <select onChange={e => setIntensiteetti(e.target.value)}>
+         <option value="1.3">Kevyt</option>
+         <option value="1.5">Tavallinen</option>
+         <option value="1.7">Kohtuullinen</option>
+         <option value="2">Kova</option>
+         <option value="2.2">Erittäin kova</option>
+       </select>
+     </div>
+     <div>
+       <label>Sukupuoli</label>
+       <label><input type="radio" name="sukupuoli" value="mies" defaultChecked onChange={e => setSukupuoli(e.target.value)} />Mies</label>
+       <label><input type="radio" name="sukupuoli" value="nainen" onChange={e => setSukupuoli(e.target.value)} />Nainen</label>
+     </div>
+     <div>
+       <label>Kalorit</label>
+       <output>{tulos.toFixed(0)}</output>
+     </div>
+     <button type="button" onClick={laske}>Laske</button>
+     {/* <input type="button" onClick={laske} value="Laske" /> */}
+   </form>
+  );
+}
+
+export default App;
